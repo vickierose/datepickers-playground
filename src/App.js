2fea386b449aa3b94f66737f570859f07dce7d4b
Viktoriia Chernyshuk <Viktoriia.Chernyshuk@eleks.com>
2017-11-03 12:56:54 +0200

    basic configuration

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return (
+     <div>
+        <h1>Hello World!!! {this.props.name}</h1>
+      </div>
+      );
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src'),
+    port: 4200
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'] }
+    ]
+},
+ plugins: [HtmlWebpackPluginConfig]
+}
